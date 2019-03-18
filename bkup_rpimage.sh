3b3cc703b785bd2df2be9b3f00a5b6fbedb829a5
dolorosus <github@dolorosus.de>
2019-03-18 23:21:54 +0100

    some comments

--- bkup_rpimage.sh
+++ bkup_rpimage.sh
@@ -5,6 +5,15 @@
 # mounted as a filesystem in a file, allowing for efficient incremental
 # backups using rsync
 #
+# The backup is taken while the system is up, so it's a good idea to stop
+# programs and services which modifies the filesystem and needed a consistant state
+# of their file. 
+# Especially applications which use databases needs to be stopped (and the database systems too).
+#
+#  So it's a smart idea to put all these stop commands in a script and perfom it before 
+#  starting the backup. After the backup terminates normally you may restart all stopped
+#  applications or just reboot the system. 
+#
 # 2019-03-18 Dolorosus: 
 #               add: exclusion of files below /tmp,/proc,/run,/sys and 
 #                    also the swapfile /var/swap will be excluded from backup.
